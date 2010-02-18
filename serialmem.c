b35b92e9468546e82e7e5b0df4eac4894e24940f
daniel <daniel@26505460-facd-4bfc-8453-c2f8c5650df9>
2010-02-18 17:35:21 +0000

    git-svn-id: https://www.network-innovation.de/svn/avr_rs232memory/trunk@5 26505460-facd-4bfc-8453-c2f8c5650df9

--- serialmem.c
+++ serialmem.c
@@ -2,8 +2,45 @@
 
 #include "usart.h"
 #include "shell.h"
+#include "tools.h"
+
+void testing() {
+	
+	DDRD = 0b00000010;
+	PORTD = 0b00000001; // pullup
+
+	DDRC = 0b00000111; //LEDS
+	PORTC = 0b00000010;
+
+	unsigned long fucker = 0;
+
+	while(1) {
+
+		if (fucker > 30000)	{
+			SBI(PORTC,1);
+			SBI(PORTD,1);
+		}	else {
+			CBI(PORTC,1);
+			CBI(PORTD,1);
+		}
+
+		if (fucker > 60000) fucker = 0;
+
+		fucker++;
+
+		if (PIND & 0b00000001) {
+			SBI(PORTC,0);
+		} else {
+			CBI(PORTC,0);
+		}
+	
+	}
+
+}
 
 int main(void) {
+
+	//testing();
 
 	wdt_enable(WDTO_2S);
 
@@ -17,7 +54,7 @@ int main(void) {
 
 	usart_init();
 
-	USART_TXS("MCU ready.\n");
+	//USART_TXS("MCU ready.\n");
 
 	//delayloop16(30000);
 
